54495419dc00d188e719676a5a0859c821282862
Tyler C <tyler@candeegenerations.com>
2019-02-21 12:57:16 -0500

    Fixed Batch Setup File.

--- setup.bat
+++ setup.bat
@@ -1,13 +1,33 @@
-echo
-echo "Loading App & Installing Packages..."
-echo
+@echo off
 
-cd ./app
-yarn
+echo.
+echo Loading App ^& Installing Packages...
+echo.
 
-echo
-echo "Loading Mock API & Installing Packages..."
-echo
+cd app\ && call yarn
 
-cd ../mock-api
-yarn
+echo.
+echo Loading Mock API ^& Installing Packages...
+echo.
+
+cd ..\mock-api && call yarn
+
+echo.
+echo _______________________________________________
+echo.
+echo Packages Installed Successfully.
+echo.
+echo _______________________________________________
+echo.
+echo Start the app using the following command:
+echo cd app && yarn start
+echo.
+echo _______________________________________________
+echo.
+echo Start the mock API using the following command:
+echo cd mock-api && yarn start
+echo.
+echo _______________________________________________
+echo.
+
+pause
