f7cfc74b2980443a0fa193e19fb9b908b6a709e2
Tyler C <tyler@candeegenerations.com>
2019-02-26 14:32:43 -0500

    Fixed HTTPS Issue.

--- app/src/config/config.production.js
+++ app/src/config/config.production.js
@@ -1,5 +1,5 @@
 const Config = {
-  apiUrl: 'http://candee-camp-mock-api.netlify.com/.netlify/functions/server',
+  apiUrl: 'https://candee-camp-mock-api.netlify.com/.netlify/functions/server',
 }
 
 export default Config
