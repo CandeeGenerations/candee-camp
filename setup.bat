ea1cae469567e8774422d148ca5c9d8cecf31663
Tyler C <tyler@candeegenerations.com>
2019-02-26 14:17:53 -0500

    Removed Mock API.

--- setup.bat
+++ setup.bat
@@ -7,12 +7,6 @@ echo.
 cd app\ && call yarn
 
 echo.
-echo Loading Mock API ^& Installing Packages...
-echo.
-
-cd ..\mock-api && call yarn
-
-echo.
 echo _______________________________________________
 echo.
 echo Packages Installed Successfully.
@@ -21,11 +15,6 @@ echo _______________________________________________
 echo.
 echo Start the app using the following command:
 echo cd app && yarn start
-echo.
-echo _______________________________________________
-echo.
-echo Start the mock API using the following command:
-echo cd mock-api && yarn start
 echo.
 echo _______________________________________________
 echo.
