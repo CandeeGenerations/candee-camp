93972bd346fe2e49c35a2555eca4420ed1b66db1
Tyler C <tyler@candeegenerations.com>
2019-07-09 15:17:09 -0400

    Upgraded Packages.

--- app/lint-staged.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  linters: {
-    'src/**/*.+(js|md|ts|css|sass|less|graphql|yml|yaml|json|vue)': [
-      'prettier --write',
-    ],
-    'src/**/*.+(jse)': ['eslint'],
-  },
-}
