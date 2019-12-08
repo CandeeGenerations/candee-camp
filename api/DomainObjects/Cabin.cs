608f8cfa1b2843680406bfdf2b6148a6ae03e8e8
Tyler C <tyler@candeegenerations.com>
2019-12-07 21:29:55 -0500

    feat: updated connection string and added extra fields to the cabin

--- api/DomainObjects/Cabin.cs
+++ api/DomainObjects/Cabin.cs
@@ -1,5 +1,5 @@
 using System;
-using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using CandeeCamp.API.DomainObjects.Common;
 
 namespace CandeeCamp.API.DomainObjects
@@ -10,8 +10,16 @@ namespace CandeeCamp.API.DomainObjects
         {
             CreatedDate = DateTimeOffset.UtcNow;
         }
-        public string name { get; set; }
+        
+        public string Name { get; set; }
 
         public DateTimeOffset CreatedDate { get; set; }
+
+        public DateTimeOffset UpdatedDate { get; set; }
+
+        public int CreatedBy { get; set; }
+
+        [ForeignKey("CreatedBy")]
+        public virtual User CreatedByUser { get; set; }
     }
 }
