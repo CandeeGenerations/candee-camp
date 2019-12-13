02f7833dd7eaf70b9d82b4a1a775dd65b77ff55a
Tyler C <tyler@candeegenerations.com>
2019-12-13 06:36:35 -0500

    feat: working on coupon code

---
+++ app/src/components/CodeCopy/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Typography} from 'antd'
+
+const CodeCopy = text => {
+  return (
+    <div>
+      <Typography.Text>{text}</Typography.Text>
+    </div>
+  )
+}
+
+export default CodeCopy
